39e7a453ead340b735431924f83d8fd3d7459023
Oketo Peters <ketoranks29@gmail.com>
2020-08-06 12:45:09 -0400

    Updated countdount test

---
+++ app/tests/components/Countdown.test.jsx
@@ -0,0 +1,44 @@
+var expect = require('expect');
+var React = require('react');
+var ReactDOM = require('react-dom');
+var TestUtils = require('react-addons-test-utils');
+var  $ = require('jQuery');
+
+var Countdown = require('Countdown');
+
+describe('Countdown', () => {
+    it('Should exist', () => {
+        expect(Countdown).toExist();
+    });
+
+
+    describe('handleSetCountdown', () => {
+        it('Should set state to started and countdown', (done) => {
+            var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+            countdown.handleSetCountdown(10);               
+            
+            expect(countdown.state.count).toBe(10);
+            expect(countdown.state.countdownStatus).toBe();
+    
+            setTimeout (() => {
+                expect(countdown.state.count).toBe(9);
+                done();
+            }, 1001)
+        });
+
+        it('Should never set count less than 0', (done) => {
+            var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+            countdown.handleSetCountdown(1);               
+            
+            //expect(countdown.state.count).toBe(10);
+            //expect(countdown.state.countdownStatus).toBe();
+    
+            setTimeout (() => {
+                expect(countdown.state.count).toBe(0);
+                done();
+            }, 3001)
+        });
+        });
+        
+        });
+    

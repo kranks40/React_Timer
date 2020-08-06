06cefdacf3c0c249617bd5a9ee84a41dd4cbdb61
Oketo Peters <ketoranks29@gmail.com>
2020-08-06 09:59:00 -0400

    updated coundown form

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -4,11 +4,34 @@ var CountdownForm = require('CountdownForm')
 
 var Countdown = React.createClass({
         getInitialState: function  () {
-            return {count: 0};
+            return {
+                count: 0,
+                countdownstatus: 'stopped'
+            };
+
+        },
+        componentDidUpdate: function (prevProps, prevState) {
+            if (this.state.countdownstatus != prevState.countdownstatus) {
+                switch (this.state.countdownstatus) {
+                    case 'started':
+                        this.startTimer();
+                        break;
+                }
+            }
+        },
+        startTimer: function () {
+            this.timer = setInterval(() => {
+                var newCount = this.state.count - 1;
+                this.setState({
+                    count: newCount >= 0 ? newCount: 0
+                });
+
+            }, 1000);
         },
         handleSetCountdown: function (seconds) {
             this.setState({
-                count: seconds
+                count: seconds,
+                countdownstatus: 'started'
             });
         },
     render: function () {
